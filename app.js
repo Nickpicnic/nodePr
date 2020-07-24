f35fe0571cc3a1eced17876020f815742fe45960
Nick <9052081221@mail.ru>
2020-07-25 01:42:56 +0300

    add simple post request

--- app.js
+++ app.js
@@ -20,7 +20,28 @@ app.get("/api/v1/tours", (req, res) => {
 
 app.post("/api/v1/tours", (req, res) => {
   console.log(req.body);
-  res.send("Done!");
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(201).json({
+          status: "success",
+          data: {
+            tour: newTour,
+          },
+        });
+        console.log("saved!");
+      }
+    }
+  );
 });
 
 app.listen(port, () => {
